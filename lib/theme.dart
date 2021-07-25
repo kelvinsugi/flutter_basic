16f157d94089781a3ff2de8125ebc757d918de5e
Kelvin Sugiarto <kelvin.sugiarto@totalbp.com>
2021-07-25 16:16:30 +0700

    flutter basic on mac

---
+++ lib/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color primary = Color(0xff314728);
+Color secondary = Color(0xffA9B0A6);
+Color backgroundColor = Color(0xffE5E5E5);
+Color white = Color(0xffFFFFFF);
+Color accent = Color(0xffF9B650);
